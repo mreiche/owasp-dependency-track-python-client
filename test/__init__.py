06ae3b845c7f3dcb6cf5a70942a147df5a0a83a0
Mike Reiche <mike@reiche.world>
2025-10-23 16:13:12 +0200

    Fix loading vulnerability

--- test/__init__.py
+++ test/__init__.py
@@ -1,12 +1,18 @@
+import math
+import time
 from pathlib import Path
+from typing import Callable
 
 from dotenv import load_dotenv
-from owasp_dt import Client
+
 import owasp_dt
+from owasp_dt import Client
 from test import config
+
 __base_dir = Path(__file__).parent
 
 test_project_name = "test-api"
+
 
 def create_client_from_env() -> owasp_dt.Client:
     base_url = config.reqenv("OWASP_DTRACK_URL")
@@ -19,12 +25,35 @@ def create_client_from_env() -> owasp_dt.Client:
         raise_on_unexpected_status=False,
         httpx_args={
             "proxy": config.getenv("HTTPS_PROXY", lambda: config.getenv("HTTP_PROXY", None)),
-            #"no_proxy": getenv("NO_PROXY", "")
+            # "no_proxy": getenv("NO_PROXY", "")
         }
     )
 
+
+def retry(callable: Callable, seconds: float, wait_time: float = 3):
+    retries = math.ceil(seconds / wait_time)
+    #start_date = datetime.now()
+    exception = None
+    ret = None
+    for i in range(retries):
+        try:
+            exception = None
+            ret = callable()
+            break
+        except Exception as e:
+            exception = e
+        time.sleep(wait_time)
+
+    if exception:
+        raise exception
+        #raise Exception(f"{exception} after {datetime.now()-start_date}")
+
+    return ret
+
+
 def setup_module():
     assert load_dotenv(__base_dir / "test.env")
+
 
 def teardown_module():
     pass
