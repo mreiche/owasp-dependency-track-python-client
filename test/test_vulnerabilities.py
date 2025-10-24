b362d4194aa831356a10507bb8c3012b466f3cd3
Mike Reiche <mike@reiche.world>
2025-10-24 09:20:03 +0200

    Add team tests

--- test/test_vulnerabilities.py
+++ test/test_vulnerabilities.py
@@ -9,17 +9,32 @@ from owasp_dt.models import ConfigProperty, ConfigPropertyPropertyType
 
 
 def test_trigger_vulnerabilities_update(client: owasp_dt.Client):
-    config = ConfigProperty(
+    config_property = ConfigProperty(
         group_name="task-scheduler",
         property_name="nist.mirror.cadence",
         property_value="1",
         property_type=ConfigPropertyPropertyType.NUMBER,
     )
-    resp = update_config_property.sync_detailed(client=client, body=config)
+    resp = update_config_property.sync_detailed(client=client, body=config_property)
     assert resp.status_code == 200
 
 
-@pytest.mark.depends(on=['test_trigger_vulnerabilities_update'])
+def test_enable_nvd(client: owasp_dt.Client):
+    config_property = ConfigProperty(
+        group_name="vuln-source",
+        property_name="nvd.enabled",
+        property_value="false",
+        property_type=ConfigPropertyPropertyType.BOOLEAN,
+    )
+    resp = update_config_property.sync_detailed(client=client, body=config_property)
+    assert resp.status_code == 200
+
+    config_property.property_value = "true"
+    resp = update_config_property.sync_detailed(client=client, body=config_property)
+    assert resp.status_code == 200
+
+
+@pytest.mark.depends(on=['test_trigger_vulnerabilities_update', "test_enable_nvd"])
 def test_get_vulnerabilities(client: owasp_dt.Client):
     def _get_vulnerabilities():
         resp = get_all_vulnerabilities.sync_detailed(client=client, page_size=1)
