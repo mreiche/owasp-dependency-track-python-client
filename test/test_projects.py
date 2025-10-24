5d3c2435c3d66ac28afb2fdeeb833a1d00f86b19
Mike Reiche <mike@reiche.world>
2025-10-24 13:58:33 +0200

    Fix test depends

--- test/test_projects.py
+++ test/test_projects.py
@@ -6,7 +6,7 @@ from owasp_dt.api.metrics import get_project_current_metrics
 from owasp_dt.api.project import get_projects
 
 
-#@pytest.mark.depends(on=['test/test_upload.py::test_upload_sbom'])
+@pytest.mark.depends(on=['test/test_upload.py::test_upload_sbom'])
 def test_search_project_by_name(client: owasp_dt.Client):
     resp = get_projects.sync_detailed(client=client, name=test.project_name)
     projects = resp.parsed
@@ -14,7 +14,7 @@ def test_search_project_by_name(client: owasp_dt.Client):
     assert projects[0].uuid is not None
     test.project_uuid = projects[0].uuid
 
-#@pytest.mark.depends(on=['test/test_upload.py::test_get_scan_status', 'test_search_project_by_name'])
+@pytest.mark.depends(on=['test/test_upload.py::test_get_scan_status', 'test_search_project_by_name'])
 def test_get_project_metrics(client: owasp_dt.Client):
     resp = get_project_current_metrics.sync_detailed(client=client, uuid=test.project_uuid)
     metrics = resp.parsed
