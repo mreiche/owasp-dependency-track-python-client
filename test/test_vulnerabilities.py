9c024c6f6cd22e2f1fcb466e3fad8eb528893d8d
Mike Reiche <mike@reiche.world>
2026-09-09 19:35:23 +0200

    feat: Integrate NVD proxy mirror (#12)
    * feat: Integrate NVD proxy mirror
    
    * fix: Status code

--- test/test_vulnerabilities.py
+++ test/test_vulnerabilities.py
@@ -4,9 +4,26 @@ import owasp_dt
 import test
 from owasp_dt.api.metrics import get_vulnerability_metrics
 from owasp_dt.api.vulnerability import get_all_vulnerabilities
+from owasp_dt_v2.api.extensions import update_extension_config
 from owasp_dt_v2.api.vuln_data_sources import trigger_vuln_data_source_mirror_run
+from owasp_dt_v2.models import UpdateExtensionConfigRequest, UpdateExtensionConfigRequestConfig
 
 
+def test_change_nvd_mirror(client_v2: owasp_dt.Client):
+    config = {
+        "enabled": True,
+        "cveFeedsUrl": "https://nvd.reiche.world/feeds"
+    }
+    request_body = UpdateExtensionConfigRequest(config=UpdateExtensionConfigRequestConfig.from_dict(config))
+    resp = update_extension_config.sync_detailed(
+        client=client_v2,
+        extension_point_name="vuln-data-source",
+        extension_name="nvd",
+        body=request_body
+    )
+    assert resp.status_code in [200, 204, 304]
+
+@pytest.mark.depends(on=['test_change_nvd_mirror'])
 def test_trigger_mirror_nvd(client_v2: owasp_dt.Client):
     resp = trigger_vuln_data_source_mirror_run.sync_detailed(client=client_v2, name="nvd")
     assert resp.status_code in [202, 409]
